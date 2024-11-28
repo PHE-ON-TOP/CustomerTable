b6d08ad7b32844d84460c9241b78f9cc0e99dd83
PHOOMPHAT <phomphat2011@gmail.com>
2024-11-28 11:59:49 +0700

    script.js

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-const correctPassword = "28/12";
-
-function checkPassword() {
-    const password = document.getElementById("password").value;
-    const errorMessage = document.getElementById("errorMessage");
-    const passwordSection = document.getElementById("passwordSection");
-    const customerData = document.getElementById("customerData");
-
-    if (password === correctPassword) {
-        passwordSection.style.display = "none";
-        customerData.style.display = "block";
-        sumPrices();
-    } else {
-        errorMessage.style.display = "block";
-        setTimeout(() => {
-            errorMessage.style.opacity = 1;
-        }, 10);
-        setTimeout(() => {
-            errorMessage.style.opacity = 0;
-        }, 2000);
-        setTimeout(() => {
-            errorMessage.style.display = "none";
-        }, 3000);
-    }
-}
-
 function sumPrices() {
     const rows = document.querySelectorAll("#customerTable tbody tr");
     let total = 0;
@@ -38,3 +12,5 @@ function sumPrices() {
 
     document.getElementById("totalPrice").textContent = total.toLocaleString();
 }
+
+sumPrices();
